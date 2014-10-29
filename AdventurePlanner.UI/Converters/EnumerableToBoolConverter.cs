7ac10bd9e085089770880ed40ddfff056ec775e3
Aaron Dorheim-Davis <aaron.bdd@gmail.com>
2014-10-28 19:39:49 -0700

    Collapse ASIs and Skill Profs sections when they are empty

---
+++ AdventurePlanner.UI/Converters/EnumerableToBoolConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AdventurePlanner.UI.Converters
+{
+    [ValueConversion(typeof(IEnumerable<object>), typeof(bool))]
+    public class EnumerableToBoolConverter : IValueConverter
+    {
+        #region Implementation of IValueConverter
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var enumerable = (IEnumerable<object>)value;
+
+            return enumerable.Any();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
