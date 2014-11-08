1e73241b81152e66c194551a593a6e3a50837c85
Aaron Dorheim-Davis <aaron.bdd@gmail.com>
2014-11-08 14:23:56 -0800

    .md -> .adoc prep pt 2. Also missed the post-build project before

---
+++ PostBuild/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using AdventurePlanner.Core;
+using AdventurePlanner.Core.Planning;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Schema;
+
+namespace Build
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var bin = AppDomain.CurrentDomain.BaseDirectory;
+
+            var solutionDir = Path.GetFullPath(Path.Combine(bin, "../../../"));
+
+            var schemaDirectory = Path.Combine(solutionDir, "docs", "schemas");
+            var examplesDirectory = Path.Combine(solutionDir, "docs", "examples");
+
+            CleanDirectory(schemaDirectory);
+            GenerateSchema(schemaDirectory, typeof(CharacterPlan));
+
+            CreateSnapshots(examplesDirectory);
+        }
+
+        private static void CreateSnapshots(string examplesDirectory)
+        {
+            foreach (var planFile in Directory.EnumerateFiles(examplesDirectory, "*.apc"))
+            {
+                var planFileBaseName = Path.GetFileNameWithoutExtension(planFile);
+                var plan = JsonConvert.DeserializeObject<CharacterPlan>(File.ReadAllText(planFile));
+
+                var numLevelPlans = plan.LevelPlans.Count;
+                var levelsToSnapshot = from n in new[] { 1, 5, 10, 20 }
+                                       where n <= numLevelPlans
+                                       select n;
+
+                foreach (var levelNum in levelsToSnapshot)
+                {
+                    var snapshot = plan.ToSnapshot(levelNum);
+
+                    var fileName = Path.Combine(
+                        examplesDirectory,
+                        string.Format("{0}-at-level-{1}.txt", planFileBaseName, levelNum));
+
+                    File.WriteAllText(fileName, snapshot.ToText());
+                }
+            }
+        }
+
+        private static void CleanDirectory(string directory)
+        {
+            Directory.EnumerateFiles(directory).ToList().ForEach(File.Delete);
+        }
+
+        private static void GenerateSchema(string docsDirectory, Type type)
+        {
+            var generator = new JsonSchemaGenerator();
+
+            var schema = generator.Generate(type);
+
+            var writer = new StringWriter();
+            var jsonTextWriter = new JsonTextWriter(writer) { Formatting = Formatting.Indented };
+
+            schema.WriteTo(jsonTextWriter);
+
+            var schemaName = schema.Title.Replace(' ', '-').ToLowerInvariant();
+            var schemaPath = Path.Combine(docsDirectory, schemaName + ".schema.json");
+
+            File.WriteAllText(schemaPath, writer.ToString());
+        }
+    }
+}
--- TODO.md
+++ TODO.md
@@ -40,7 +40,7 @@ Source TODOs
     * 67 :question: Consider moving ToSnapshot into an extension method.
     * 93 :poop: This is really clunky
 * \AdventurePlanner.UI\ViewModels\CharacterPlanViewModel.cs
-    * 42 Consider: Instead of calling AddLevel on create, instead call SetFromPlan() with initial state.
+    * 44 Consider: Instead of calling AddLevel on create, instead call SetFromPlan() with initial state.
 * \AdventurePlanner.UI\ViewModels\ClassPlanViewModel.cs
     * 36 :bug: Not running when loading a file. I ran into this issue before, but can't remember where or how I got around it. Look into dispatchers.
 * \AdventurePlanner.UI\ViewModels\FeaturePlanViewModel.cs
