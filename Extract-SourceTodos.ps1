891ecf605c5c62975cfbae230aaa0e2bb9025329
Aaron Dorheim-Davis <aaron.bdd@gmail.com>
2014-11-14 23:04:10 -0800

    Link to TODO line

--- Extract-SourceTodos.ps1
+++ Extract-SourceTodos.ps1
@@ -12,7 +12,7 @@ $todos = gci $PSScriptRoot -r `
         | ? { $_.FullName -notmatch $excludePattern } `
         | select-string $todoMark `
         | % { new-object PSObject `
-                -Prop @{ Path = $_.Path.Replace($PSScriptRoot, ''); `
+                -Prop @{ Path = $_.Path.Replace($PSScriptRoot, '').Replace('\', '/'); `
                          LineNum = $_.LineNumber; `
                          Task = $_.Line.SubString($_.Line.IndexOf($todoMark) + $todoMark.Length + 1).Trim() `
                        } }
@@ -31,9 +31,9 @@ foreach ($todo in $todos)
     if ($file -ne $todo.Path)
     {
         $file = $todo.Path
-        $cleanTodoLines += "* $($todo.Path)"
+        $cleanTodoLines += "* link:.$($todo.Path)[]"
     }
-    $cleanTodoLines += "** $($todo.LineNum) $($todo.Task)"
+    $cleanTodoLines += "** link:.$($todo.Path)#L$($todo.LineNum)[$($todo.LineNum)] $($todo.Task)"
 }
 
 $encoding = New-Object System.Text.UTF8Encoding($true)
